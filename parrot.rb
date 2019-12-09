811890493287f678612d25512461ad3c06844cb8
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-09 20:04:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
+def parrot(phrase)
   puts "Squawk!"
   return "Squawk!"
 end
 
-
+phrase="Pretty bird!"
+parrot
